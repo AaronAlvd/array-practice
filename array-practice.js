12f8c22bd2ecdaf67c1c305a09ddb334e31016bb
Aaron <offxcxal.aaron@icloud.com>
2024-05-11 09:10:33 -0700

    initial commit

---
+++ array-practice.js
@@ -0,0 +1,34 @@
+const findMinimum = arr => {
+  
+};
+
+const runningSum = arr => {
+ 
+};
+
+const evenNumOfChars = arr => {
+  
+};
+
+const smallerThanCurr = arr => {
+    
+};
+
+const twoSum = (arr, target) => {
+
+  // Your code here 
+};
+
+const secondLargest = arr => {
+
+  // Your code here 
+};
+
+const shuffle = (arr) => {
+
+  // Your code here 
+};
+
+
+module.exports = [findMinimum, runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
+
